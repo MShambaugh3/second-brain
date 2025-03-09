4970e8651e381e09ae53379fdb26b2efbc6cbd37
MShambaugh3 <max.shambaugh3@gmail.com>
2025-03-08 20:01:25 -0800

    Configuired Next.js for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
